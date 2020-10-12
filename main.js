6c81179330e5a838314cd1a17c235c08b861bafa
Arnavkheterpal12 <71542068+Arnavkheterpal12@users.noreply.github.com>
2020-10-12 20:54:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,14 +10,24 @@ recognition.onresult= function(event){
     console.log(Content);
     speak();
     document.getElementById("textbox").innerHTML=Content;
+    if(Content=="take my selfie"){
+        console.log("taking selfie in 5 seconds");
+        speak();
+    }
     
 }
 function speak(){
     var synth= window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value;
+    speak_data="Taking your selfie in 5 seconds";
     var utterThis=new SpeechSynthesisUtterance(speak_data);
+    
     synth.speak(utterThis);
-    Webcam.attach(camera)
+    Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+
+    },5000);
 }
 Webcam.set({
     width:360,
@@ -27,3 +37,15 @@ Webcam.set({
 
 });
 camera=document.getElementById("camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">';
+
+    });
+}
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image.src");
+    link.href=image;
+    link.click();
+}
